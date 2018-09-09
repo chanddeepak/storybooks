00d87d9d2be6dc393d359d687cd7826ed33c3535
Deepak Chand <deepu996gmail.com>
2018-09-09 16:04:43 +0530

    version 1.0.0alpha done

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://deepak:deepak987@ds239692.mlab.com:39692/mystorybook",
-  googleClientID:
-    "726668144350-894dkdn8mo5jqm9jn19equdl3jluqq2p.apps.googleusercontent.com",
-  googleClientSecret: "JgEoQyTBx7THGJH3Wt69r5QX"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: procees.env.MONGO_URI,
+  googleClientID: procees.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: procees.env.GOOGLE_CLIENT_SECRET
+};
